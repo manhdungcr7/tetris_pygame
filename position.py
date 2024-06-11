826b18761912cd2a18abc17f611d6a6ff64ad87b
manhdungcr7 <158057151+manhdungcr7@users.noreply.github.com>
2024-06-11 23:14:35 +0700

    Add files via upload

---
+++ position.py
@@ -0,0 +1,14 @@
+class Position:
+    def __init__(self, row, column):
+        """
+        Khởi tạo đối tượng Position với hàng và cột xác định.
+
+        Args:
+            row (int): Chỉ số hàng.
+            column (int): Chỉ số cột.
+        """
+        self.row = row
+        self.column = column
+
+
+help(Position)
